f5bfa5386aa537570b6579cda518267fed690434
Soyab Padhrashi <SoyabPadhrashi9627@gmail.com>
2025-10-21 13:35:07 +0530

    commit

---
+++ src/main/java/com/example/soyaboplSpring/SoyaboplSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.soyaboplSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoyaboplSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoyaboplSpringApplication.class, args);
+	}
+
+}
