9d7820411162a9426e1a01191ea92a4e8f0d5167
Soyab8200 <soyabpadhrashi9627@gmail.com>
2025-10-21 15:27:54 +0530

    Update TestDto.java
    xx

--- src/main/java/com/example/soyaboplSpring/dto/TestDto.java
+++ src/main/java/com/example/soyaboplSpring/dto/TestDto.java
@@ -3,5 +3,14 @@ package com.example.soyaboplSpring.dto;
 public class TestDto {
 private int id;
   private int hu;
-  
+  private int kokok;
+
+private Integer sss;
+  private char jj;
+  private String www;
+private String hihihll;
+  private Double tdtdt;
+
+  private Integer nmnm;
+
 }
