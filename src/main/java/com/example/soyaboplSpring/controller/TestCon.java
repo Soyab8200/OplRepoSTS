37b11015856bf3f28ab97b2408ce49856a841781
Soyab Padhrashi <SoyabPadhrashi9627@gmail.com>
2025-10-21 23:57:16 +0530

    controller

--- src/main/java/com/example/soyaboplSpring/controller/TestCon.java
+++ src/main/java/com/example/soyaboplSpring/controller/TestCon.java
@@ -1,13 +1,21 @@
 package com.example.soyaboplSpring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.soyaboplSpring.service.TestInter;
 
 @RestController
 public class TestCon {
 	
+	@Autowired
+	private TestInter testInter;
+	
+	
 	public String getAll()
 	{
-		return "hello";
+		String hello = testInter.getHello();
+		return hello;
 	}
 
 }
